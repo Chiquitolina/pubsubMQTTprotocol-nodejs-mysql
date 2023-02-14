19018643f8d0491bdaf14df355292f9eed39d649
Alejandro <aleig.906@gmail.com>
2023-02-14 15:51:36 -0300

    db insertions

--- pub.js
+++ pub.js
@@ -3,12 +3,14 @@ let client = mqtt.connect('mqtt://broker.hivemq.com')
 
 client.on("connect", () => {
     setInterval(() => {
-        let random = Math.random()*25
+        let random = Math.round(Math.random()*25)
         console.log('El número generado es:', random);
         if (random >= 20) {
+            const fecha = new Date (Date.now()).toLocaleString()
             const obj = {
                 numero: random,
-                date: new Date (Date.now()).toLocaleDateString()
+                date: fecha.slice(0, 9),
+                hour: fecha.slice(10 , fecha.length)
             }
             client.publish('Numerospana', JSON.stringify(obj))
             console.log('Número publicado:', JSON.stringify(obj));
--- sub.js
+++ sub.js
@@ -2,8 +2,7 @@ const mqtt = require('mqtt')
 const conexionaDB = require('./database')
 let client = mqtt.connect('mqtt://broker.hivemq.com')
 let topic = "Numerospana"
-
-conexionaDB()
+let last
 
 client.on('connect', () => {
     client.subscribe(topic)
@@ -11,5 +10,14 @@ client.on('connect', () => {
 })
 
 client.on('message', (topic, message) => {
-    console.log('Esuchando el ' + message)
+    last = JSON.parse(message)
+    console.log('Esuchando el ' + last.numero + ', publicado el ' + last.date + ' a las' + last.hour)
 })
+
+setInterval(() => {
+conexionaDB().query('INSERT INTO tabla (numero, fecha, hora) VALUES (?, ?, ?)', [last.numero, last.date, last.hour], (error, results, fields) => {
+    if (error) throw error;
+    console.log('Datos insertados correctamente.');
+  })
+}, 200000)
+
